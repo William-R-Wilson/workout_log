7eb81220a0fd1796560ce8bc673a05d329e13962
Will <williamrwilson@gmail.com>
2016-11-24 18:03:18 -0500

    testing gitignore

---
+++ server/smtp.js
@@ -0,0 +1,11 @@
+Meteor.startup(function(){
+console.log("smtp settings initialized");
+  smtp = {
+    username: 'test5',   // eg: server@gentlenode.com
+    password: 'test5',   // eg: 3eeP1gtizk5eziohfervU
+    server:   'smtp.gmail.com',  // eg: mail.gandi.net
+    port: 25
+  }
+
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+});
