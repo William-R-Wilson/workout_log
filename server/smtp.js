c794f02994211e9b5c99e5367499586cbf8a7ca2
Will <williamrwilson@gmail.com>
2016-11-24 17:40:46 -0500

    test gitignore

--- server/smtp.js
+++ server/smtp.js
@@ -1,8 +1,8 @@
 Meteor.startup(function(){
 
 smtp = {
-  username: 'test2',   // eg: server@gentlenode.com
-  password: 'test2',   // eg: 3eeP1gtizk5eziohfervU
+  username: 'test3',   // eg: server@gentlenode.com
+  password: 'test3',   // eg: 3eeP1gtizk5eziohfervU
   server:   'smtp.gmail.com',  // eg: mail.gandi.net
   port: 25
 }
